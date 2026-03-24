71f588c75f5924a1392a99a7c5a9238bec202a97
Camilo Vera <camilo@fvv.cl>
2026-03-24 17:15:17 -0300

    ghidra, ghidra-bin: 12.0.2 -> 12.0.4
    Update ghidra and ghidra-bin to 12.0.4 and add caverav as a maintainer for both packages.
    
    Release notes: https://github.com/NationalSecurityAgency/ghidra/releases/tag/Ghidra_12.0.4_build

--- pkgs/tools/security/ghidra/default.nix
+++ pkgs/tools/security/ghidra/default.nix
@@ -28,11 +28,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "ghidra";
-  version = "12.0.2";
-  versiondate = "20260129";
+  version = "12.0.4";
+  versiondate = "20260303";
   src = fetchzip {
     url = "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_${version}_build/ghidra_${version}_PUBLIC_${versiondate}.zip";
-    hash = "sha256-E2edj1psLK1hrKCDcjQbQZaYls8iq5ZG3vdwSUUJts8=";
+    hash = "sha256-O+Y1k1fRVTZVJMBA62gyqWw0KZOwrBqqRC3dlhQHbhY=";
   };
 
   nativeBuildInputs = [
@@ -85,6 +85,7 @@ stdenv.mkDerivation rec {
     sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [
+      caverav
       ck3d
       govanify
       mic92
